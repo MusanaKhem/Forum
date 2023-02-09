bd55d27510c4470da127ab8f5e21f9e9711f9fde
MusanaKhem <hermann.acm@gmail.com>
2023-02-10 00:25:05 +0100

    add files & modified signup.php

--- signup.php
+++ signup.php
@@ -2,43 +2,43 @@
 <html lang="en">
 <?php include 'includes/head.php'; ?>
 <body>
-
-<form>
+<br></br>
+<form class="container" method="POST">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
 
 
   <div class="mb-3">
     <label for="exampleInputPseudo" class="form-label">Pseudo</label>
-    <input type="text" class="form-control" id="exampleInputPseudo" aria-describedby="emailHelp">
+    <input type="text" class="form-control" name="pseudo" id="exampleInputPseudo" aria-describedby="emailHelp">
   </div>
 
 
   <div class="mb-3">
     <label for="exampleInputName" class="form-label">Name</label>
-    <input type="text" class="form-control" id="exampleInputName" aria-describedby="emailHelp">
+    <input type="text" class="form-control" name="lastname" id="exampleInputName" aria-describedby="emailHelp">
   </div>
-  
+
   <div class="mb-3">
     <label for="exampleInputFirstname" class="form-label">Firstname</label>
-    <input type="text" class="form-control" id="exampleInputFirstname" aria-describedby="emailHelp">
+    <input type="text" class="form-control" name="firstname" id="exampleInputFirstname" aria-describedby="emailHelp">
   </div>  
 
 
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
   </div>
 
 
   <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <input type="checkbox" class="form-check-input" name="checkbox" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" name="validate">Submit</button>
 </form>
 
 </body>
