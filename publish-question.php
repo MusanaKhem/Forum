c258be2ae023dce00a3a2cc122e692540db0082f
MusanaKhem <hermann.acm@gmail.com>
2023-02-17 00:11:42 +0100

    publish-question better coded

---
+++ publish-question.php
@@ -0,0 +1,37 @@
+<?php 
+    require('actions/users/securityAction.php');
+    require('actions/questions/publishQuestionAction.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'; ?>
+<body>
+    <?php include 'includes/navbar.php'; ?>
+    <br></br>
+
+    <!-- PUBLISH-QUESTION FORM -->
+    <form class="container" method="POST">
+
+        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+
+        <div class="mb-3">
+            <label for="exampleInputQuestionObject" class="form-label">Title or object</label>
+            <input type="text" class="form-control" name="object">
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleInputQuestionDescription" class="form-label">Description</label>
+            <textarea type="text" class="form-control" name="description"></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleInputQuestionContent" class="form-label">Content</label>
+            <textarea type="text" class="form-control" name="content"></textarea>
+        </div>  
+
+    <button type="submit" class="btn btn-primary" name="validate">Publier la question</button><br></br>
+
+	</form>
+
+</body>
+</html>
