e113464198b679a742ebde92c4623e54b8f9b878
MusanaKhem <hermann.acm@gmail.com>
2023-02-17 13:58:37 +0100

    add_publish_question_file

--- publish-question.php
+++ publish-question.php
@@ -12,16 +12,22 @@
     <!-- PUBLISH-QUESTION FORM -->
     <form class="container" method="POST">
 
-        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+        <?php 
+            if(isset($errorMsg)){
+                echo '<p>'.$errorMsg.'</p>'; 
+            }elseif(isset($successMsg)){
+                echo '<p>'.$successMsg.'</p>';
+            }
+        ?>
 
         <div class="mb-3">
             <label for="exampleInputQuestionObject" class="form-label">Title or object</label>
-            <input type="text" class="form-control" name="object">
+            <input type="text" class="form-control" name="title">
         </div>
 
         <div class="mb-3">
             <label for="exampleInputQuestionDescription" class="form-label">Description</label>
-            <textarea type="text" class="form-control" name="description"></textarea>
+            <textarea type="text" class="form-control" name="explanation"></textarea>
         </div>
 
         <div class="mb-3">
