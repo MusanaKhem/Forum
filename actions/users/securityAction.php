d5e88e3bb1563a60c629a06cc1a02715eb1dc0a9
MusanaKhem <hermann.acm@gmail.com>
2023-02-18 01:27:28 +0100

    users

--- actions/users/securityAction.php
+++ actions/users/securityAction.php
@@ -1,6 +1,5 @@
 <!-- securityAction's part's code -- redirection to login page if user isn't authenticated -->
 <?php
-session_start();
 if(!isset($_SESSION['auth'])){
     header('Location: login.php');
 }
