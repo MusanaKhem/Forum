405cf621b75dfe21fdeecd00f036c0bae9f96dca
MusanaKhem <hermann.acm@gmail.com>
2023-02-18 01:34:42 +0100

    update actions, questions, myQuestion

--- actions/questions/myQuestionsAction.php
+++ actions/questions/myQuestionsAction.php
@@ -3,4 +3,4 @@
 require('actions/database.php'); //
 
 $getAllMyQuestions = $bdd->prepare('SELECT id, titre, explanation FROM questions WHERE id_author = ?');
-$getAllMyQuestions->exectue(array($_SESSION['id']));
+$getAllMyQuestions->execute(array($_SESSION['id']));
