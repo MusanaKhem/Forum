ae3e6ef391c574e9119bb1d73bfbbd7cc4dc5dc6
MusanaKhem <hermann.acm@gmail.com>
2023-02-20 20:12:08 +0100

    show all questions into index page

---
+++ actions/questions/showAllQuestionAction.php
@@ -0,0 +1,17 @@
+<?php
+
+require('actions/database.php');
+
+// Recover all information questions without user's search
+$getAllQuestions = $bdd->query('SELECT id, id_author, title, explanation, pseudo_author, publish_datetime FROM questions ORDER BY id DESC LIMIT 0,5');
+
+// Verify if search bar is not empty and if user has already complete and enter its search
+if(isset($_GET['search']) AND !empty($_GET['search'])){
+
+    // Get user research
+    $userSearch = $_GET['search'];
+
+    // Compare user entry to titles which are in database
+    $getAllQuestions = $bdd->query('SELECT id, id_author, title, explanation, pseudo_author, publish_datetime FROM questions WHERE title LIKE "%'.$userSearch.'%" ORDER BY id DESC');
+}
+
